2d919273e251ea1e71f12e0125ed12684974432a
Anubhav Gupta <53078155+GuptaAnubhav1@users.noreply.github.com>
2020-05-19 17:52:20 +0530

    Day 8

--- 8.Dictioneries&Maps.py
+++ 8.Dictioneries&Maps.py
@@ -5,11 +5,15 @@ import re
 n = int(input())
 phonebook = {}
 for i in range(n):
-    key_value = input().split()
+    key_value = input().strip().split(" ")
     phonebook[key_value[0]] = key_value[1]
-for j in range(n):
-    x= input()
-    if(x in phonebook):
-        print(x,"=",phonebook[x], sep="")
-    else:
-        print("Not found")
+for j in range(n):    
+                
+    while True:
+        try:
+            x= input()
+            if(x in phonebook):
+                print("{}={}".format(x,phonebook[x]))
+            else:
+                print("Not found")
+        except: quit()

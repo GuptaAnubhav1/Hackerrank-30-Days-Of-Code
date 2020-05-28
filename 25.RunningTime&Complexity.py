b58a190d5b51b642f873f4be4dd83f74c6993fa3
Anubhav Gupta <anubhav.gupta5@mail.dcu.ie>
2020-05-29 03:59:09 +0530

    Day 25

---
+++ 25.RunningTime&Complexity.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import math
+import re
+
+
+for _ in range(int(input())):
+    num = int(input())
+    if(num == 1):
+        print("Not prime")
+    else:
+        if(num % 2 == 0 and num > 2):
+            print("Not prime")
+        else:
+            for i in range(3, int(num**(1/2))+1, 2):
+                if num % i == 0:
+                    print("Not prime")
+                    break
+            else:
+                print("Prime")
